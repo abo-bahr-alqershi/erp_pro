648fbd29caa635cb5a1f50c2b3c9ad2e05b5df07
ameen alqershi <ameen@kali>
2025-04-30 01:53:32 +0300

    18 commit

--- ERP_Pro.Domain/ERP/Other/Entities/OutgoingAccounts.cs
+++ ERP_Pro.Domain/ERP/Other/Entities/OutgoingAccounts.cs
@@ -1,31 +1,43 @@
 public class OutgoingAccounts
 {
-    public int? OutType { get; private set; }
-    public string? GCode { get; private set; }
-    public long? WhgCode { get; private set; }
-    public string? ACode { get; private set; }
-    public int? AdUId { get; private set; }
-    public DateTime? AdDate { get; private set; }
-    public int? UpUId { get; private set; }
-    public DateTime? UpDate { get; private set; }
-    public long? UpCnt { get; private set; }
-    public long? PrRep { get; private set; }
-    public string? AdTrmnlNm { get; private set; }
-    public string? UpTrmnlNm { get; private set; }
-    public string? MngCode { get; private set; }
-    public string OutName { get; private set; }
-    public string? OutEName { get; private set; }
-    public bool? OutSr { get; private set; }
+    /// <summary> نوع المصروف </summary>
+    public int? OutgoingType { get; private set; }
+    /// <summary> كود المجموعة </summary>
+    public string? GroupCode { get; private set; }
+    /// <summary> كود المستودع </summary>
+    public long? WarehouseGroupCode { get; private set; }
+    /// <summary> كود الحساب </summary>
+    public string? AccountCode { get; private set; }
+    /// <summary> المستخدم الذي أضاف </summary>
+    public int? AddedUserId { get; private set; }
+    /// <summary> تاريخ الإضافة </summary>
+    public DateTime? AddedDate { get; private set; }
+    /// <summary> المستخدم الذي عدل </summary>
+    public int? UpdatedUserId { get; private set; }
+    /// <summary> تاريخ التعديل </summary>
+    public DateTime? UpdatedDate { get; private set; }
+    /// <summary> عدد مرات التحديث </summary>
+    public long? UpdateCount { get; private set; }
+    /// <summary> أولوية التقرير </summary>
+    public long? ReportPriority { get; private set; }
+    /// <summary> اسم الجهاز عند الإضافة </summary>
+    public string? AddedTerminal { get; private set; }
+    /// <summary> اسم الجهاز عند التعديل </summary>
+    public string? UpdatedTerminal { get; private set; }
+    /// <summary> كود المدير </summary>
+    public string? ManagerCode { get; private set; }
+    /// <summary> اسم المصروف </summary>
+    public string? OutgoingName { get; private set; }
+    /// <summary> اسم المصروف بالإنجليزي </summary>
+    public string? OutgoingEName { get; private set; }
+    /// <summary> علم خاص بالمصروف </summary>
+    public bool? OutgoingSr { get; private set; }
+    /// <summary> تحويل إلى أصول </summary>
     public bool? ToAssets { get; private set; }
-    public bool? OutRes { get; private set; }
-    public bool? ConnWithSi { get; private set; }
-    public int? AdUId { get; private set; }
-    public DateTime? AdDate { get; private set; }
-    public int? UpUId { get; private set; }
-    public DateTime? UpDate { get; private set; }
-    public long? UpCnt { get; private set; }
-    public long? PrRep { get; private set; }
-    public string? AdTrmnlNm { get; private set; }
-    public string? UpTrmnlNm { get; private set; }
-    public bool? OutPms { get; private set; }
+    /// <summary> علم خاص بالمصروف </summary>
+    public bool? OutgoingRes { get; private set; }
+    /// <summary> مرتبط بفواتير المبيعات </summary>
+    public bool? ConnectedWithSalesInvoice { get; private set; }
+    /// <summary> علم خاص بالمشاريع </summary>
+    public bool? OutgoingPms { get; private set; }
 }
